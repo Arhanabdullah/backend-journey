143cfd413d315501812b5f4bf5af264938223d45
Arhan Abdullah <arhan.abdullah307@gmail.com>
2026-03-02 23:46:43 +0530

    Added Comments for RestAPI Fundamentals

--- ExpressJS/server.js
+++ ExpressJS/server.js
@@ -5,3 +5,8 @@ app.get('/', (req, res) => {
     res.send("Hello World")
 })
 app.listen(3000)
+
+// GET => server se data fetch krna ho tbb use krte h
+// POST => server me data send krna ho tbb use krte h
+// PATCH => server me data already h aur uss data ko update krna ho toh patch use krte h
+// DELETE => server me data already h aur uss data ko delete krna ho 
