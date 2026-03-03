96f35d80a40481b59a7de71f1e2e58694867ca6b
Arhan Abdullah <arhan.abdullah307@gmail.com>
2026-03-03 23:40:55 +0530

    Used RestAPI Methods

---
+++ FolderSturcturee/FolderSturcture/app.js
@@ -0,0 +1,45 @@
+// app.js is used to create and initiate the express app and export it to be used in other files like server.js
+
+const express = require("express")
+
+const app = express()
+
+app.use(express.json()) // to parse the incoming request body as JSON
+
+const notes = []
+
+app.post('/notes', (req, res) => {
+    notes.push(req.body)
+    res.status(201).json({
+        message: "Note created successfully"
+    })
+})
+
+app.get('/notes', (req, res) => {
+    res.status(200).json({
+        notes: notes,
+        message: "Notes Fetched Successfully"
+    })
+})
+
+app.delete('/notes/:index', (req, res) => {
+    const index = req.params.index
+    delete notes[index]
+    res.status(200).json({
+        message: "Note Deleted Successfully"
+    })
+})
+
+
+
+app.patch('/notes/:index', (req, res) => {
+    const index = req.params.index
+    const description = req.body.description 
+    notes[index].description = description
+
+    res.status(200).json({
+        message: "Note Updated Successfully"
+    })
+
+})
+module.exports = app
---
+++ FolderSturcturee/server.js
@@ -0,0 +1,7 @@
+
+const app = require('./FolderSturcture/app')
+
+app.listen(3000,()=>{
+    console.log("Server is running");
+    
+})
