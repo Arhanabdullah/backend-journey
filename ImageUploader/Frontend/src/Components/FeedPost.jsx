84661208191943f67ffb7d62bb17ef2f235630a2
Arhan Abdullah <arhan.abdullah307@gmail.com>
2026-03-15 23:55:23 +0530

    Created create post and feed page

---
+++ ImageUploader/Frontend/src/Components/FeedPost.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+const FeedPost = () => {
+
+    const [posts, setposts] = useState([])
+    useEffect(()=>{
+        axios.get('http://localhost:3000/posts')
+        .then((res)=>{
+            console.log(res.data);
+            
+        })
+    },[])
+    return (
+        <section className='feed-post-section'>
+            {
+            posts.length > 0 ?
+            posts.map((post) => {
+                return (
+                    <div className='feed-post'>
+                        <img src={post.image} alt={post.caption} />
+                        <p>{post.caption}</p>
+                    </div>
+                )
+            })
+            : <h2>No Posts Available</h2>
+        }
+
+        </section>
+    )
+}
+
+export default FeedPost
