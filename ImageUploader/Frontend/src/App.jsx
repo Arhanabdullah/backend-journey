1429f38413520d89523184a13c2c56df4fb08538
Arhan Abdullah <arhan.abdullah307@gmail.com>
2026-03-16 23:48:44 +0530

    Added Styling

--- ImageUploader/Frontend/src/App.jsx
+++ ImageUploader/Frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import CreatePost from './Components/CreatePost'
+import FeedPost from './Components/FeedPost'
 const App = () => {
     return (
         <div>
             <Routes>
-                <Route path='/' element={<h2>Home Page</h2>}></Route>
-                <Route path='/about' element={<h2>About Page</h2>}></Route>
+                <Route path='/' element={<CreatePost />}></Route>
+                <Route path='/feed' element={<FeedPost />}></Route>
             </Routes>
         </div>
     )
