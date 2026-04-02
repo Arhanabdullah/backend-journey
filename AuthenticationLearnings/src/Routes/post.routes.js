0d1950dec3d3abaf8fcc2741654b0c2c3516ae51
Arhan Abdullah <arhan.abdullah307@gmail.com>
2026-04-02 23:42:34 +0530

    Create Post model

---
+++ AuthenticationLearnings/src/Routes/post.routes.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const userModel = require('../models/user.model');
+
+router.post('/create', async (req, res) => {
+    const token = req.cookies.token;
+
+    if(!token){
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+        const user = await userModel.findOne({
+            _id: decoded.id
+        })
+        return res.status(201).json({
+            message: "Post created successfully",
+            user
+        })
+    } catch (err) {
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+        
+    }
+
+})  
+module.exports = router;
