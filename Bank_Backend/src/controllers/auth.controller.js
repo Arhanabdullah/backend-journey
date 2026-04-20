7ba1eb2a94e3435ce446572a74ffabb15de3c0bf
Arhan Abdullah <arhan.abdullah307@gmail.com>
2026-04-20 23:55:16 +0530

    Started with Controller

--- Bank_Backend/src/controllers/auth.controller.js
+++ Bank_Backend/src/controllers/auth.controller.js
@@ -1,5 +1,19 @@
 const userModel = require('../models/user.model')
 
 async function register(req, res) {
-    
+    const { name, email, password } = req.body
+
+    const alreadyExist = await userModel.findOne({ email })
+
+    if (alreadyExist) {
+        return res.status(400).json({ message: 'User already exists' })
+    }
+    const user = new userModel({
+        name,
+        email,
+        password
+    })
+    await user.save()
+    res.status(201).json({ message: 'User created successfully' })
+
 }
