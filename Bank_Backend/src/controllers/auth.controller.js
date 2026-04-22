64dc3a7a1cdd29dbe033a12f121471b1092445fd
Arhan Abdullah <arhan.abdullah307@gmail.com>
2026-04-22 23:27:23 +0530

    Added Access Token

--- Bank_Backend/src/controllers/auth.controller.js
+++ Bank_Backend/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 const userModel = require('../models/user.model')
+const jwt = require('jsonwebtoken')
 
 async function register(req, res) {
     const { name, email, password } = req.body
@@ -8,12 +9,21 @@ async function register(req, res) {
     if (alreadyExist) {
         return res.status(400).json({ message: 'User already exists' })
     }
-    const user = new userModel({
+    const user = await userModel.create({
         name,
         email,
         password
     })
+    const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' }, (err, token) => {
+        if (err) {
+            return res.status(500).json({ message: 'Error generating token' })
+        }        user.token = token
+    })
+    
     await user.save()
-    res.status(201).json({ message: 'User created successfully' })
+
+    res.status(201).json({ message: 'User created successfully' ,
+        accessToken
+    })
 
 }
