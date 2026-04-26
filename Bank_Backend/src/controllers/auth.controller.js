386d95e32c735e7d43656f56e7fa7e08daa8655f
Arhan Abdullah <arhan.abdullah307@gmail.com>
2026-04-26 23:48:17 +0530

    Created register API

--- Bank_Backend/src/controllers/auth.controller.js
+++ Bank_Backend/src/controllers/auth.controller.js
@@ -7,23 +7,32 @@ async function register(req, res) {
     const alreadyExist = await userModel.findOne({ email })
 
     if (alreadyExist) {
-        return res.status(400).json({ message: 'User already exists' })
+        return res.status(422).json({ message: 'User already exists', status: "Failed" })
     }
     const user = await userModel.create({
         name,
         email,
         password
     })
-    const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' }, (err, token) => {
-        if (err) {
-            return res.status(500).json({ message: 'Error generating token' })
-        }        user.token = token
-    })
+    const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' })
+        res.cookie('accessToken', accessToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',  
+            sameSite: 'strict',
+            maxAge: 24 * 60 * 60 * 1000 
+        })
     
+
     await user.save()
 
-    res.status(201).json({ message: 'User created successfully' ,
-        accessToken
+    res.status(201).json({
+        user:{
+            _id: user._id,
+            name: user.name,
+            email: user.email
+        },
+        message: 'User created successfully',
+        status: "Success", accessToken
     })
 
-}
+} 
