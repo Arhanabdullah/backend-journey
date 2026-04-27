3a318babca550a881ee90817d01b5131af7187e5
Arhan Abdullah <arhan.abdullah307@gmail.com>
2026-04-27 23:59:01 +0530

    Added Login API

--- Bank_Backend/src/controllers/auth.controller.js
+++ Bank_Backend/src/controllers/auth.controller.js
@@ -36,3 +36,25 @@ async function register(req, res) {
     })
 
 } 
+
+async function login(req, res) {
+    const { email, password } = req.body    
+    const user = await userModel.findOne({ email })
+
+    if (!user) {
+        return res.status(404).json({ message: 'User not found', status: "Failed" })
+    }   
+    const isMatch = await user.comparePassword(password)
+
+    if (!isMatch) {
+        return res.status(401).json({ message: 'Invalid credentials', status: "Failed" })
+    }   
+    const accessToken = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' })
+        res.cookie('accessToken', accessToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',  
+            sameSite: 'strict',
+            maxAge: 24 * 60 * 60 * 1000 
+        })
+    }
+
